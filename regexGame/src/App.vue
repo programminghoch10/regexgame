22c4d1f5830d4b6dfbda7e14ae6fac5021509ac6
leonleoff <76099309+leonleoff@users.noreply.github.com>
2022-05-17 20:06:56 +0200

    Export AnswerBox

--- regexGame/src/App.vue
+++ regexGame/src/App.vue
@@ -1,91 +1,46 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import {Question} from './components/question';
+import AnswerBox from './components/AnswerBox.vue';
 
-
-export default defineComponent({
+export default {
   name: 'App',
-  components: {},
+  components: {
+    AnswerBox
+  },
   props: {
     question : Question
   },
   data() {
     return {
-
     }
   },
-  methods: {
-    selectAnswer(name: string ){
-    // this.$refs[name].classList.add("flip-card-inner-animation");
-    },
-    mounted(){
-      
-    }
-  }
-});
+  methods: {},
+    mounted(){}
+  };
 </script>
+
 
 <template>
   <div class="gamebox">
     <div class="headerbox">
+      
       <h1>RegEx Game</h1>
       <div class="regexbox"><h2 class="regex" id="regex">d|a(b)*c</h2></div>
       <div class="row" id="rowbox">
-        <div class="col-4 answerbox" @click="selectAnswer('answer0')">
-          <div class="flip-card">
-            <div class="flip-card-inner" ref="answer0">
-              <div class="flip-card-front">
-                <h3>ac</h3>
-              </div>
-              <div class="flip-card-back">
-                <div class="wrong-flip-card-back">Wrong</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-4 answerbox"  @click="selectAnswer('answer1')">
-          <div class="flip-card">
-            <div class="flip-card-inner" ref="answer1">
-              <div class="flip-card-front">
-                <h3>dabbbc</h3>
-              </div>
-              <div class="flip-card-back">
-                <div class="wrong-flip-card-back">
-                  Wrong
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-4 answerbox" @click="selectAnswer('answer2')">
-          <div class="flip-card">
-            <div class="flip-card-inner" ref="answer2">
-              <div class="flip-card-front">
-                <h3>dbcc</h3>
-              </div>
-              <div class="flip-card-back">
-                <div class="wrong-flip-card-back">
-                  Wrong
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <answer-box></answer-box>
+        <answer-box></answer-box>
+        <answer-box></answer-box>
       </div>
       <h4>Points: 5 {{}}</h4>
     </div>
   </div>
 </template>
 
+
 <style>
 @import "@/assets/base.css";
-
-
-
-#app {
-  
-}
-
+#app {}
 .gamebox {
   margin-top: 20%;
   margin-left: 20%;
@@ -109,65 +64,5 @@ export default defineComponent({
 #rowbox {
   margin-right: 0px;
   margin-left: 0px;
-}
-
-.answerbox {
-  height: 200px;
-}
-/* Answer Card Style */
-
-.flip-card {
-  border: solid black 2px;
-  background-color: transparent;
-  width: 100%;
-  height: 100%;
-  perspective: 1000px; /* Remove this if you don't want the 3D effect */
-}
-
-/* This container is needed to position the front and back side */
-.flip-card-inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  transition: transform 0.8s;
-  transform-style: preserve-3d;
-  margin-top: 50%;
-
-}
-
-/* Do an horizontal flip when you move the mouse over the flip box container */
- .flip-card-inner-animation {
-  transform: rotateY(180deg);
-}
-
-/* Position the front and back side */
-.flip-card-front,
-.flip-card-back {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  -webkit-backface-visibility: hidden; /* Safari */
-  backface-visibility: hidden;
-}
-
-/* Style the front side (fallback if image is missing) */
-.flip-card-front {
-  color: black;
-}
-
-/* Style the back side */
-.flip-card-back {
-  transform: rotateY(180deg);
-}
-
-.wrong-flip-card-back {
-  background-color: red;
-  color: black;
-}
-
-.right-flip-card-back {
-  background-color: green;
-  color: black;
 }
 </style>
--- regexGame/src/components/AnswerBox.vue
+++ regexGame/src/components/AnswerBox.vue
@@ -1,13 +1,98 @@
 <template>
-  
+  <div class="col-4 answerbox" @click="selectAnswer('answer0')">
+          <div class="flip-card">
+            <div class="flip-card-inner" ref="answer0">
+              <div class="flip-card-front">
+                <h3>ac</h3>
+              </div>
+              <div class="flip-card-back">
+                <div class="wrong-flip-card-back">Wrong</div>
+              </div>
+            </div>
+          </div>
+        </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
+  props: {
+    onError: Function,
+    turn: Boolean,
+  },
+  data() {
+    return {
+      cards: [],
+      score: 0,
+    };
+  },
+  mounted() {
+    this.initCards();
+  },
+  methods: {
+      selectAnswer(name: string ){
+    this.$refs[name].classList.add("flip-card-inner-animation");
+      }
+  },
+};
+</script>
+<style>
+.answerbox {
+  height: 200px;
+}
+/* Answer Card Style */
+
+.flip-card {
+  border: solid black 2px;
+  background-color: transparent;
+  width: 100%;
+  height: 100%;
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+  margin-top: 50%;
 
 }
-</script>
 
-<style>
+/* Do an horizontal flip when you move the mouse over the flip box container */
+ .flip-card-inner-animation {
+  transform: rotateY(180deg);
+}
 
+/* Position the front and back side */
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+  color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+  transform: rotateY(180deg);
+}
+
+.wrong-flip-card-back {
+  background-color: red;
+  color: black;
+}
+
+.right-flip-card-back {
+  background-color: green;
+  color: black;
+}
 </style>
