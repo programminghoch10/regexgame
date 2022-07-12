9ad14770feebf81a21967667f740bcba748beb82
Gr33nbl00d <st172032@stud.uni-stuttgart.de>
2022-07-12 20:29:50 +0200

    Update src/model/data.ts
    Co-authored-by: delvh <dev.lh@web.de>

--- src/model/data.ts
+++ src/model/data.ts
@@ -2,6 +2,4 @@ import { Quiz } from "./game";
 
 export function quizData(): Quiz[] {
   return [new Quiz(/((.ba)*a)*/, "", "ab", "laalaa")];
-  list.push(new Quiz(/((.ba)*a)*/, "", "ab", "laalaa"));
-  return list;
 }
