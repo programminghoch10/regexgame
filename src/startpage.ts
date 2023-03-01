6022591cbf105765159d804b43478e32766190d6
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2023-03-01 16:17:07 +0100

    add basic gameplay logic

--- src/startpage.ts
+++ src/startpage.ts
@@ -4,6 +4,24 @@ function quit() {
   window.close()
 }
 
-function play() {
+const exampleRiddle: Riddle = new Riddle(/ab(ab)*/, ["aba", "abb", "ababa", "abab"])
 
+function play() {
+  displayRiddle(exampleRiddle)
+}
+
+function setHidden(element: HTMLElement, hidden: boolean) {
+  element.classList[hidden ? "add" : "remove"]("hidden")
+}
+
+const startPageDiv = document.querySelector("#startpage")! as HTMLDivElement
+const gameDiv = document.querySelector("#game")! as HTMLDivElement
+function switchToStartPage() {
+  setHidden(gameDiv, true)
+  setHidden(startPageDiv, false)
+  removeCurrentRiddle()
+}
+function switchToGame() {
+  setHidden(startPageDiv, true)
+  setHidden(gameDiv, false)
 }
