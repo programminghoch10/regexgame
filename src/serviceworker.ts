46eed6bfe40f7876f25efd85eb67b5f0aad07ef8
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2024-06-15 19:45:09 +0200

    implement async network loading in serviceworker
    this will prevent the site from being forever in cache and never updating

--- src/serviceworker.ts
+++ src/serviceworker.ts
@@ -1,17 +1,24 @@
-async function cacheFirst(request: Request) {
-  const responseFromCache = await caches.match(request)
-  if (responseFromCache) return responseFromCache
+
+async function loadFromNetwork(request: Request): Promise<Response> {
   try {
-    const responseFromNetwork = await fetch(request)
+    const reponse = await fetch(request)
     const cache = await caches.open("v1")
-    await cache.put(request, responseFromNetwork.clone())
-    return responseFromNetwork
+    await cache.put(request, reponse.clone())
+    return reponse
   } catch (error) {
     return new Response("Network error happened", {
       status: 408,
       headers: { "Content-Type": "text/plain" },
     })
   }
+}
+
+async function cacheFirst(request: Request): Promise<Response> {
+  const responseFromCache = await caches.match(request)
+  if (!responseFromCache)
+    return await loadFromNetwork(request)
+  loadFromNetwork(request)
+  return responseFromCache
 }
 
 self.addEventListener("fetch", (event: any) => {
